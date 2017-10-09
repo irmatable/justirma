00cda2e8e9219f5b8d009627f57c33a8067239fc
irmattg <irmaamesa@me.com>
2017-10-09 15:40:46 -0400

    completed site

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typed3 = new Typed('#typed3', {
+    strings: ['I\'m a QA Tester.', 'I\'m a developer.', 'I \'m a risk-taker.', 'I\'m Irma, nice to virtually meet you.'],
+    typeSpeed: 40,
+    backSpeed: 30,
+    contentType: 'text',
+    smartBackspace: true, // this is a default
+  });
+    
